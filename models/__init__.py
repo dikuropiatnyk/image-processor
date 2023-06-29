da1c56b48394d4798ae1ef328ec8291093320673
Danylo Kuropiatnyk <dikuropiatnyk@spscommerce.com>
2023-06-29 17:50:55 +0300

    Remove OpenIO models

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 from .db_document import DBImage
 from .encoded_image import DefaultWatermark, EncodedImage, EncodedImages
-from .openio import OpenioCreateResponse, OpenioImage
 from .output import (
     OutputImage,
     OutputWatermark,
