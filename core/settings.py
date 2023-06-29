e7247cb185e713dea0687ffcaa9ab319ab18dd4e
Danylo Kuropiatnyk <dikuropiatnyk@spscommerce.com>
2023-06-30 01:49:47 +0300

    Prepare kubernetes deployment
    - added docker-compose file for local development
    - added Dockerfile for image

--- core/settings.py
+++ core/settings.py
@@ -1,18 +1,31 @@
-from pydantic import BaseSettings
+import os
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
     DB_NAME: str = "processor"
     IMAGE_CONTAINER: str = "images"
     WATERMARK_CONTAINER: str = "watermarks"
-    MONGO_USER: str
-    MONGO_PWD: str
-    MONGO_URL: str
     WATERMARK_HEIGHT: int = 100
     WATERMARK_WIDTH: int = 100
+    MONGO_USER: str = Field(env="MONGO_USER")
+    MONGO_PWD: str = Field(env="MONGO_PWD")
+    MONGO_URL: str = Field(env="MONGO_URL")
 
 
-settings = Settings(
-    _env_file=".env.secrets",
-    _env_file_encoding="utf-8"
-)
+def get_settings():
+    env = os.getenv("ENV")
+    if env == "local":
+        _settings = Settings(
+            _env_file=".env.secrets",
+            _env_file_encoding="utf-8"
+        )
+    elif env == "remote":
+        _settings = Settings()
+    else:
+        raise RuntimeError(f'Insufficient environment parameter: "{env}"')
+
+    return _settings
+
+
+settings = get_settings()
